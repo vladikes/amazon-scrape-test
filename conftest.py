983145622087812228eed64732cc20e631bc7209
vladikes <vladikesler@gmail.com>
2019-01-15 03:37:08 +0200

    removed a comment

--- conftest.py
+++ conftest.py
@@ -4,19 +4,7 @@ from selenium import webdriver
 
 chrome_driver_path = str(Path.cwd() / "chromedriver")
 
-
-# @pytest.fixture(scope="session")
-# def driver_init():
-#     from selenium import webdriver
-#     web_driver = webdriver.Chrome(chrome_driver_path)
-#     web_driver.implicitly_wait(3)
-#     web_driver.maximize_window()
-#     # request.cls.driver = web_driver
-#     yield
-#     web_driver.close()
-#     web_driver.quit()
-
-@pytest.fixture(scope="class")
+pytest.fixture(scope="class")
 def driver_init(request):
     print("initiating chrome driver")
     driver = webdriver.Chrome(chrome_driver_path)
@@ -26,4 +14,4 @@ def driver_init(request):
 
     yield driver
     driver.close()
-    driver.quit()
+    driver.quit() 
